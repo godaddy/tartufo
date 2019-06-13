cf593c6ca853166a199de735426943e0b4bd27b3
Mark Henry <mxhenry@godaddy.com>
2019-06-13 10:17:00 -0700

    Don't deploy when on a PR to master

--- scripts/build_and_deploy_wheel.sh
+++ scripts/build_and_deploy_wheel.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-python3 setup.py sdist bdist_wheel
-python3 -m twine upload -u $PYPI_USER -p $PYPI_PASS dist/*
+if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
+    python3 setup.py sdist bdist_wheel
+    python3 -m twine upload -u $PYPI_USER -p $PYPI_PASS dist/*
+else
+    echo "Skipping deployment as we are on a Pull Requst"
+fi
