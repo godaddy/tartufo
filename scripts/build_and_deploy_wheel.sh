6474fe98a24b3b1420a7131dcb7ed43681bf7207
Mark Henry <mxhenry@godaddy.com>
2019-06-13 09:47:32 -0700

    install required packages prior to script execution

--- scripts/build_and_deploy_wheel.sh
+++ scripts/build_and_deploy_wheel.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-python3 -m pip install --user --upgrade setuptools wheel
 python3 setup.py sdist bdist_wheel
-python3 -m pip install --user --upgrade twine
 python3 -m twine upload -u $PYPI_USER -p $PYPI_PASS --repository-url https://pypi.org/project/gd-truffleHog dist/*
