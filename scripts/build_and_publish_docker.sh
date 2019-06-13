2f10545c1472fe0f20d70b07cae50b864cd82354
Mark Henry <mxhenry@godaddy.com>
2019-06-13 10:57:24 -0700

    using --password-stdin

--- scripts/build_and_publish_docker.sh
+++ scripts/build_and_publish_docker.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "docker_build_and_publish" ]]; then
-    docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
     docker build -t trufflehog .
     docker images
     docker tag trufflehog $DOCKER_USERNAME/trufflehog
-    docker push $DOCKER_USERNAME/trufflehog:latest
+#    docker push $DOCKER_USERNAME/trufflehog:latest
 else
     echo "Skipping publish as we are on a Pull Request"
 fi
