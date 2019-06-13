39af8d37e7b30847cc894583df8cd8a511d0e4da
Mark Henry <mxhenry@godaddy.com>
2019-06-13 09:55:54 -0700

    don't need url for upload?

--- scripts/build_and_deploy_wheel.sh
+++ scripts/build_and_deploy_wheel.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 python3 setup.py sdist bdist_wheel
-python3 -m twine upload -u $PYPI_USER -p $PYPI_PASS --repository-url https://pypi.org/project/gd-truffleHog dist/*
+python3 -m twine upload -u $PYPI_USER -p $PYPI_PASS dist/*
