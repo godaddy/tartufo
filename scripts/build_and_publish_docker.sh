344e7b01594cd0af070436d5e87556ea47783a13
Mark Henry <mxhenry@godaddy.com>
2019-06-13 11:02:04 -0700

    push to docker

--- scripts/build_and_publish_docker.sh
+++ scripts/build_and_publish_docker.sh
@@ -4,7 +4,7 @@ if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "docker_build_a
     docker build -t trufflehog .
     docker images
     docker tag trufflehog $DOCKER_USERNAME/trufflehog
-#    docker push $DOCKER_USERNAME/trufflehog:latest
+    docker push $DOCKER_USERNAME/trufflehog:latest
 else
     echo "Skipping publish as we are on a Pull Request"
 fi
