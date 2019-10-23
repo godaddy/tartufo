5455dd51bc55feb7058bb084c4b35ff2a9c0c0c7
Joey Wilhelm <jwilhelm@godaddy.com>
2019-10-23 16:03:09 -0400

    Better ensure we're always in the proper CWD

--- scripts/build_and_publish_docker.sh
+++ scripts/build_and_publish_docker.sh
@@ -3,10 +3,13 @@
 IMAGE_NAMESPACE=godaddy
 IMAGE_NAME=tartufo
 
+# Move into the scripts folder
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 IMAGE_VERSION=$(cat ../VERSION | tr -d '[:space:]')
 IMAGE_FULL_NAME=${IMAGE_NAMESPACE}/${IMAGE_NAME}
 
+# Go back to the top level of the project
+cd ..
 # Log in to Docker Hub
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 # Build and tag as the "latest" version of the image
