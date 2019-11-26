529972f7296fdfa9ad4c5509d38dadd2f467073b
Joey Wilhelm <jwilhelm@godaddy.com>
2019-11-26 11:51:42 -0500

    Adjust tests for the new code

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,12 +1,12 @@
 from __future__ import unicode_literals
 
+import copy
 import os
 import re
 import unittest
 
 import click
 from click.testing import CliRunner
-from truffleHogRegexes.regexChecks import regexes as default_regexes
 
 from tartufo import config
 
@@ -15,17 +15,23 @@ try:
 except ImportError:
     import pathlib2 as pathlib  # type: ignore
 
+try:
+    from unittest import mock
+except ImportError:
+    import mock  # type: ignore
+
 
 class ConfigureRegexTests(unittest.TestCase):
-    def test_configure_regexes_from_args_rules_files_without_defaults(self):
+    def test_configure_regexes_rules_files_without_defaults(self):
         rules_path = pathlib.Path(__file__).parent / "data" / "testRules.json"
         rules_files = (rules_path.open(),)
         expected_regexes = {
             "RSA private key 2": re.compile("-----BEGIN EC PRIVATE KEY-----")
         }
 
-        args = {"regex": True, "default_regexes": False, "rules": rules_files}
-        actual_regexes = config.configure_regexes_from_args(args, default_regexes)
+        actual_regexes = config.configure_regexes(
+            include_default=False, rules_files=rules_files
+        )
 
         self.assertEqual(
             expected_regexes,
@@ -34,16 +40,17 @@ class ConfigureRegexTests(unittest.TestCase):
             "(expected: {}, actual: {})".format(expected_regexes, actual_regexes),
         )
 
-    def test_configure_regexes_from_args_rules_files_with_defaults(self):
+    def test_configure_regexes_rules_files_with_defaults(self):
         rules_path = pathlib.Path(__file__).parent / "data" / "testRules.json"
         rules_files = (rules_path.open(),)
-        expected_regexes = dict(default_regexes)
+        expected_regexes = copy.copy(config.DEFAULT_REGEXES)
         expected_regexes["RSA private key 2"] = re.compile(
             "-----BEGIN EC PRIVATE KEY-----"
         )
 
-        args = {"regex": True, "default_regexes": True, "rules": rules_files}
-        actual_regexes = config.configure_regexes_from_args(args, default_regexes)
+        actual_regexes = config.configure_regexes(
+            include_default=True, rules_files=rules_files
+        )
 
         self.assertEqual(
             expected_regexes,
@@ -52,29 +59,55 @@ class ConfigureRegexTests(unittest.TestCase):
             "(expected: {}, actual: {})".format(expected_regexes, actual_regexes),
         )
 
-    def test_configure_regexes_from_args_no_do_regex(self):
-        rules_path = pathlib.Path(__file__).parent / "data" / "testRules.json"
-        rules_files = (rules_path.open(),)
-        args = {"regex": False, "default_regexes": True, "rules": rules_files}
-        actual_regexes = config.configure_regexes_from_args(args, default_regexes)
+    def test_configure_regexes_returns_just_default_regexes_by_default(self):
+        actual_regexes = config.configure_regexes()
 
         self.assertEqual(
-            {},
-            actual_regexes,
-            "The regexes dictionary should be empty when do_regex is False",
-        )
-
-    def test_configure_regexes_from_args_no_rules(self):
-        expected_regexes = dict(default_regexes)
-
-        args = {"regex": True, "default_regexes": True, "rules": ()}
-        actual_regexes = config.configure_regexes_from_args(args, default_regexes)
-
-        self.assertEqual(
-            expected_regexes,
+            config.DEFAULT_REGEXES,
             actual_regexes,
             "The regexes dictionary should not have been changed when no rules files are specified",
         )
+
+    @mock.patch("tartufo.config.util.clone_git_repo")
+    def test_configure_regexes_does_not_clone_if_local_rules_repo_defined(
+        self, mock_clone
+    ):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            config.configure_regexes(rules_repo=".")
+        mock_clone.assert_not_called()
+
+    @mock.patch("tartufo.config.util.clone_git_repo")
+    def test_configure_regexes_clones_git_rules_repo(self, mock_clone):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            mock_clone.return_value = pathlib.Path(".").resolve()
+            config.configure_regexes(rules_repo="git@github.com:godaddy/tartufo.git")
+        mock_clone.assert_called_once_with("git@github.com:godaddy/tartufo.git")
+
+    @mock.patch("tartufo.config.pathlib")
+    def test_configure_regexes_grabs_all_json_from_rules_repo_by_default(
+        self, mock_pathlib
+    ):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            repo_path = mock_pathlib.Path.return_value.resolve.return_value
+            repo_path.is_dir.return_value = True
+            repo_path.glob.return_value = []
+            config.configure_regexes(rules_repo=".")
+            repo_path.glob.assert_called_once_with("*.json")
+
+    @mock.patch("tartufo.config.pathlib")
+    def test_configure_regexes_grabs_specified_rules_files_from_repo(
+        self, mock_pathlib
+    ):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            repo_path = mock_pathlib.Path.return_value.resolve.return_value
+            repo_path.is_dir.return_value = True
+            repo_path.glob.return_value = []
+            config.configure_regexes(rules_repo=".", rules_repo_files=("tartufo.json",))
+            repo_path.glob.assert_called_once_with("tartufo.json")
 
 
 class ConfigFileTests(unittest.TestCase):
