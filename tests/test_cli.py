fad2f61b2730d47fba2bd2a99b83643bbe5cfb5e
Joey Wilhelm <tarkatronic@gmail.com>
2020-11-09 16:17:28 -0700

    Partial fix for Windows pathing test issue

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -99,9 +99,11 @@ class ProcessIssuesTest(unittest.TestCase):
             result = runner.invoke(
                 cli.main, ["--output-dir", "./foo", "scan-local-repo", "."]
             )
+        output_dir = (
+            Path(dirname) / "foo" / "tartufo-scan-results-nownownow"
+        ).resolve()
         self.assertEqual(
-            result.output,
-            f"Results have been saved in {Path(dirname).resolve()}/foo/tartufo-scan-results-nownownow\n",
+            result.output, f"Results have been saved in {output_dir}\n",
         )
 
     @unittest.skipUnless(platform.system().lower() == "windows", "Test is Windows-only")
