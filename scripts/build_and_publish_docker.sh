9ff57580deafae4843a5d19fd5b70848e064ce17
Sushant Mimani <sushant.mimani@gmail.com>
2021-10-12 15:45:27 -0700

    Update latest image on Docker Hub only if it is a stable release

--- scripts/build_and_publish_docker.sh
+++ scripts/build_and_publish_docker.sh
@@ -6,15 +6,18 @@ IMAGE_NAME=tartufo
 # Move into the scripts folder
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 IMAGE_VERSION=$(awk -F'[ ="]+' '$1 == "version" { print $2 }' ../pyproject.toml)
+VERSION_SUFFIX=$(echo ${IMAGE_VERSION} |  awk -F '-' '{print $2}')
 IMAGE_FULL_NAME=${IMAGE_NAMESPACE}/${IMAGE_NAME}
 
 # Go back to the top level of the project
 cd ..
 # Log in to Docker Hub
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-# Build and tag as the "latest" version of the image
-docker build -t ${IMAGE_FULL_NAME}:latest .
-# Give the build a more specific version tag, from the VERSION file
-docker tag ${IMAGE_FULL_NAME}:latest ${IMAGE_FULL_NAME}:${IMAGE_VERSION}
-docker push ${IMAGE_FULL_NAME}:latest
+# Build, tag & publish image with the specific version tag
+docker build -t ${IMAGE_FULL_NAME}:${IMAGE_VERSION} .
 docker push ${IMAGE_FULL_NAME}:${IMAGE_VERSION}
+# If this is a stable release(no version suffix), update latest docker image
+if [  "${VERSION_SUFFIX}" == "" ]; then
+    docker tag ${IMAGE_FULL_NAME}:${IMAGE_VERSION} ${IMAGE_FULL_NAME}:latest
+    docker push ${IMAGE_FULL_NAME}:latest
+fi
