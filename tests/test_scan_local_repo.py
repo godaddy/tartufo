ed8ce81f03b1472a754b938f0dbcf03bece1d993
jwilbur-godaddy <103447557+jwilbur-godaddy@users.noreply.github.com>
2022-05-20 14:05:56 -0400

    Add tests for #352

--- tests/test_scan_local_repo.py
+++ tests/test_scan_local_repo.py
@@ -1,13 +1,13 @@
 import unittest
 from pathlib import Path
 from unittest import mock
-
 from click.testing import CliRunner
-
 from tartufo import cli, types
-
 from tests import helpers
-
+from hashlib import sha256
+from pygit2 import Repository
+from subprocess import call
+from os import remove
 
 class ScanLocalRepoTests(unittest.TestCase):
     @mock.patch("tartufo.commands.scan_local_repo.GitRepoScanner")
@@ -32,3 +32,27 @@ class ScanLocalRepoTests(unittest.TestCase):
                 str(result.exception),
                 f"Repository not found at {Path(run_path).resolve()}",
             )
+
+    def test_new_file_shows_up(self):
+        runner = CliRunner()
+        # Add file with high entropy
+        secret_key = sha256(b"hello world")
+        f = open("secret.key", "a")
+        f.write(secret_key.hexdigest())
+        f.close()
+        repo = Repository(".")
+
+        # Check that tartufo picks up on newly added files
+        repo.index.add("tests/data/config/secret.key")
+        repo.index.write() # This actually writes the index to disk. Without it, the tracked file is not actually staged.
+        result_1 = runner.invoke(cli.main, ["--entropy-sensitivity", "1", "pre-commit"])
+        self.assertNotEqual(result_1.exit_code, 0)
+
+        # Check that tartufo does not flag non-added new files
+        repo.index.remove("tests/data/config/secret.key")
+        repo.index.write() # This actually writes the index to disk. Without it, secret.key will not return to being untracked.
+        result_2 = runner.invoke(cli.main, ["--entropy-sensitivity", "1", "pre-commit"])
+        self.assertEqual(result_2.exit_code, 0)
+
+        # Cleanup
+        remove("secret.key")
